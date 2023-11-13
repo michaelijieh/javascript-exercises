921ef0fd7ec28ad3b9b8e973715c0bbcd441fe55
michaelijieh <michaelijieh@gmail.com>
2023-11-13 18:20:33 +0100

    Exercises 1 to 3 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return "ERROR"
+    }
+        
+    let words = "";
+    for (i = 1; i <= number; i++) {
+        words += word;
+    }
+    return words
+
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(words) {
+
+let strings = words;
+let lStrings = strings.length;
+let newWord = "";
+
+let i = lStrings;
+while (i >= 0) {
+    newWord += strings.charAt(i)
+
+    i--;
+}
+
+return newWord;
+
+
+
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
