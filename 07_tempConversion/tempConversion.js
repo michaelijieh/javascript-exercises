38e1f60182131b16dd25be9397e4dab601701878
michaelijieh <michaelijieh@gmail.com>
2023-11-14 19:03:15 +0100

    Exercise 6 code cleaned up. Exercises 7 and 8 completed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32)/(9/5)
+  if (Number.isInteger(temp)) {
+    return temp
+  } else {
+    return Number(temp.toFixed(1))
+  }
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 9/5) +32
+  if (Number.isInteger(temp)) {
+    return temp
+  } else {
+    return Number(temp.toFixed(1))
+  }
+  
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,47 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let total = 0;
+
+  for (const number of array) {
+    total += number
+  }
+  return total
+
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
+
+  for (const number of array) {
+    total *= number
+  }
+  return total
+
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  let result = Math.pow(a, b);
+  return result
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  let total = 1;
+  for (let i = 1; i <= number; i++) {
+    total *= i
+  }
+  return total
 	
 };
 
@@ -31,3 +54,5 @@ module.exports = {
   power,
   factorial
 };
+
+
