bad51d1a0d2b646c93eefd14cb955bc8daa9e726
michaelijieh <michaelijieh@gmail.com>
2023-11-14 15:43:34 +0100

    Exercises 4 and 5 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arrays, ...theArgs) {
+    let newElenents = [];
+    for (const array of arrays) {
+        if(!theArgs.includes(array)) {
+            newElenents.push(array)
+            
+        }
+            
+    } return newElenents
+    
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
-};
+    if (typeof(num1) === "number" && typeof(num2) === "number") {
+
+        if (num1 < 0 || num2 < 0) {
+            return "ERROR"
+        } else {
+
+            let numDiff = num1 - num2
+
+            if (numDiff < 0){
+                let total = 0;
+                for (let i = num1; i <= num2; i++) {
+                    total += i;
+                }
+                return total 
+            
+            } else if (numDiff > 0) {
+                let total = 0;
+                for (let i = num2; i <= num1; i++) {
+                    total += i;
+                }
+                return total
+
+            } else {
+                let total = 0;
+                total = num1 + num2
+                return total
+            }
+
+
+        }
+    } else {
+        return "ERROR"
+    }
+
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
